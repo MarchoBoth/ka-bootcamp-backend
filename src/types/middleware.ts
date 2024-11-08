0305991e6a7d717aa93bdf26f09b73f47a973b07
MarchoBoth <anjasariesta01.com>
2024-11-08 18:06:36 +0800

    tokoKomputer

--- src/types/middleware.ts
+++ src/types/middleware.ts
@@ -1,13 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
 import React from "react";
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   if (!token) {
     return NextResponse.redirect(new URL("/auth/signin", request.url));
+  } else {
+    return NextResponse.next();
   }
-  return NextResponse.next();
 }
-export const config = {
-  matcher: [],
+export const config: MiddlewareConfig = {
+  matcher: ["/", "/product/:path*", "/categories/:path*"],
 };
