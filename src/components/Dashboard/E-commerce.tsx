f83c2f6f6508e10f909b70b7acaa9ece34a785c0
MarchoBoth <anjasariesta01.com>
2024-12-25 10:15:07 +0800

    coba

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -167,12 +167,12 @@ const ECommerce = ({
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         {/* <ChartOne /> */}
         {/* <ChartTwo profit={profit} /> */}
-        <ChartThree categories={categories} />
+        {/* <ChartThree categories={categories} /> */}
         {/* <MapOne /> */}
         <div className="col-span-12">
-          <TableOne products={topProducts} />
+          {/* <TableOne products={topProducts} /> */}
         </div>
-        <ChatCard />
+        {/* <ChatCard /> */}
       </div>
     </>
   );
