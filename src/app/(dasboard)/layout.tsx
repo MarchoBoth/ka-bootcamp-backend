2f9a35e11d532975e4fcf6f990475f1d2ac4c7bf
MarchoBoth <anjasariesta01.com>
2024-12-26 14:52:54 +0800

    duration

--- src/app/(dasboard)/layout.tsx
+++ src/app/(dasboard)/layout.tsx
@@ -1,6 +1,7 @@
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { revalidatePath } from "next/cache";
 export const revalidate = 0;
+export const maxDuration = 60;
 export const metadata = {
   title: "Toko Komputer",
   description: "Dashboard",
