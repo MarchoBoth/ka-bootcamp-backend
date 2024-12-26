fbb1bd746b57e0ecea87a527a612610f62647cbb
MarchoBoth <anjasariesta01.com>
2024-12-26 14:38:57 +0800

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,11 +21,11 @@ export async function middleware(request: NextRequest) {
   }
 
   const token = request.cookies.get("token")?.value;
-  // if (!token) {
-  //   return NextResponse.redirect(new URL("/auth/signin", request.url));
-  // } else {
-  //   return NextResponse.next();
-  // }
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  } else {
+    return NextResponse.next();
+  }
 }
 export const config: MiddlewareConfig = {
   matcher: [
